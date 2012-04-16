ac63f66731a3e253c29d7ebf21dde0328d88c739
SangChan Lee <gyaleon@paran.com>
2012-04-16 18:05:29 +0900

    Initial Commit

---
+++ baggar/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  baggar
+//
+//  Created by SangChan Lee on 12. 4. 16..
+//  Copyright (c) 2012년 KTH Corp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ baggar/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  baggar
+//
+//  Created by SangChan Lee on 12. 4. 16..
+//  Copyright (c) 2012년 KTH Corp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
